2ea7ca6e4255bd3015770e95c64ee168a3bbe09a
Pponghub <wallop.wwww@gmail.com>
2025-10-05 07:49:47 +0700

    test discord bot

---
+++ index.js
@@ -0,0 +1,16 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+const { token } = require("./config.json");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.on("ready", (c) => {
+  console.log(`Logged in as ${c.user.tag}`);
+});
+
+client.login(token);
